d6746350f8d6e940d99e2a8f26bfdb5928c7a60b
Richie Patil <142727189+sourcerich@users.noreply.github.com>
2024-12-30 19:04:47 +0530

    routes

---
+++ gamecom-backend/src/modules/reviews/routes/review.route.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response, NextFunction } from "express";
+
+const router = Router();
+router.use(function (req: Request, res: Response, next: NextFunction) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept",
+  );
+  next();
+});
+/*router.get("/kpi", (req: Request, res: Response) => {
+  kpiOrder(req, res);
+});*/
+
+export const review = router;
